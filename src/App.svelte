f9f700a7a52fc1337f1d1bded9fae878d41d71f1
Jeff Ruoss <jeffreyruoss@gmail.com>
2023-07-30 14:48:52 -0700

    Set up vite-plugin-pwa and Service Worker.

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,20 @@
 <script>
 import Test from './components/Test.svelte';
+import { onMount } from 'svelte';
+
+onMount(async () => {
+  if ('serviceWorker' in navigator) {
+    try {
+      const registration = await navigator.serviceWorker.register('/sw.js');
+      console.log('Service Worker Registered:', registration);
+    } catch (registrationError) {
+      console.log('Service Worker Registration Failed:', registrationError);
+    }
+  } else {
+    console.log('Service Worker is not supported in this browser');
+  }
+});
+
 </script>
 
 <div class="container">
