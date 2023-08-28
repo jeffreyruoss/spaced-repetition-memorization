edb58fd67999d873ecf7dc5ee184ff8a6524ffc1
Jeff Ruoss <jeffreyruoss@gmail.com>
2023-08-28 15:12:55 -0400

    Try to fix vercel.

--- .vercel/output/functions/fn.func/.svelte-kit/vercel-tmp/manifest.js
+++ .vercel/output/functions/fn.func/.svelte-kit/vercel-tmp/manifest.js
@@ -10,7 +10,7 @@ return {
 	assets: new Set(["android-chrome-192x192.png","android-chrome-512x512.png","apple-touch-icon.png","favicon-16x16.png","favicon-32x32.png","favicon.ico","favicon.png","global.css","green_circle_192.png","green_circle_512.png","site.webmanifest","sw.dev.js"]),
 	mimeTypes: {".png":"image/png",".ico":"image/vnd.microsoft.icon",".css":"text/css",".webmanifest":"application/manifest+json",".js":"application/javascript"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.f147dbdc.js","app":"_app/immutable/entry/app.741b8fb8.js","imports":["_app/immutable/entry/start.f147dbdc.js","_app/immutable/chunks/scheduler.f585318a.js","_app/immutable/chunks/index.a9296cf4.js","_app/immutable/chunks/control.f5b05b5f.js","_app/immutable/entry/app.741b8fb8.js","_app/immutable/chunks/scheduler.f585318a.js","_app/immutable/chunks/index.bc0e1543.js"],"stylesheets":[],"fonts":[]},
+		client: {"start":"_app/immutable/entry/start.c40085da.js","app":"_app/immutable/entry/app.741b8fb8.js","imports":["_app/immutable/entry/start.c40085da.js","_app/immutable/chunks/scheduler.f585318a.js","_app/immutable/chunks/index.a9296cf4.js","_app/immutable/chunks/control.f5b05b5f.js","_app/immutable/entry/app.741b8fb8.js","_app/immutable/chunks/scheduler.f585318a.js","_app/immutable/chunks/index.bc0e1543.js"],"stylesheets":[],"fonts":[]},
 		nodes: [
 			__memo(() => import('../output/server/nodes/0.js')),
 			__memo(() => import('../output/server/nodes/1.js')),
